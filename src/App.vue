fb36cefcbe34bae9d36cd113acbaf58447a4379b
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-25 16:25:08 +0100

    film summary function added

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,24 @@ export default {
   name: 'app',
   data() {
     return {
-      allFilms: []
+      allFilms: [],
+      allFilmsSummary: []
     }
   },
   mounted() {
     fetch('https://ghibliapi.herokuapp.com/films')
     .then(response => response.json())
-    .then(rawFilmData => this.allFilms = rawFilmData)
+    .then((rawFilmData) => {
+      this.allFilms = rawFilmData;
+      this.allFilmsSummary = this.getFilmSummaries(this.allFilms)
+    })
+  },
+  methods: {
+    getFilmSummaries(films) {
+      return films.map(function (film) {
+        return {'title': film.title, 'description': film.description}
+      });
+    }
   },
   components: {
 
@@ -27,11 +38,11 @@ export default {
 
 <style>
 /* #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+font-family: 'Avenir', Helvetica, Arial, sans-serif;
+-webkit-font-smoothing: antialiased;
+-moz-osx-font-smoothing: grayscale;
+text-align: center;
+color: #2c3e50;
+margin-top: 60px;
 } */
 </style>
