593691a89129421f66945edace3a52242423fc7e
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-25 20:31:08 +0100

    select film button and eventBus added

--- src/components/filmList.vue
+++ src/components/filmList.vue
@@ -3,15 +3,27 @@
     <div v-for="film of allFilms" :key="film.id">
       <h2>{{film.title}}</h2>
       <p>{{film.description}}</p>
+      <button @click="handleClick(film)">See film info</button>
     </div>
   </div>
 </template>
 
 <script>
-
+import {eventBus} from '../main.js';
 
 export default {
-  props: ["allFilms"]
+  name: 'filmList',
+  // data() {
+  //   return{
+  //     film: {}
+  //   }
+  // },
+  props: ["allFilms"],
+  methods: {
+    handleClick(film) {
+      eventBus.$emit("selected-film", film);
+    }
+  }
 }
 </script>
 
