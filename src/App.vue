6236961513b64e73452b7b131d890aa5e7a86dee
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-25 15:28:44 +0100

    film data loaded into app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>From App.vue</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
+  data() {
+    return {
+      allFilms: []
+    }
+  },
+  mounted() {
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(response => response.json())
+    .then(rawFilmData => this.allFilms = rawFilmData)
+  },
   components: {
-    HelloWorld
+
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
